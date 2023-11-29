f25648d987643326a684f7016e18548ddc2588c0
ddiieeggoo b <zkpgmi@gmail.com>
2023-11-29 14:49:46 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+
+# Categories for restaurants
+CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+# Seed 5 valid restaurants
+5.times do |index|
+  Restaurant.create!(
+    name: "Restaurant #{index + 1}",
+    address: "Address #{index + 1}",
+    phone_number: "123-456-789#{index + 1}",
+    category: CATEGORIES.sample
+  )
+end
